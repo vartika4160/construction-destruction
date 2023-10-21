e5a2a4a0f3b9925adf059aa9637e82af0c41a309
vartika4160 <139868043+vartika4160@users.noreply.github.com>
2023-10-21 23:27:52 +0530

    Create para_constructor.cpp

---
+++ para_constructor.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class construct{
+    int a,b;
+    public:
+    construct(int m,int n)
+    {
+        a = m;
+        b = n;
+    }
+    void display()
+    {
+        cout<<"a="<<a<<endl<<"b="<<b;
+    }
+};
+int main()
+{
+    construct p(10,20);
+    p.display();
+}
+/*Output
+a=10
+b=20
+*/
